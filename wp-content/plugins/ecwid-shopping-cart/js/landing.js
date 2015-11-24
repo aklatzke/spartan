6a2912e4d250db9ad411961a60e1db1236802d6f
Andrew Klatzke <andrew@klatzke.com>
2015-11-24 12:37:44 -0500

    Removed uploads & plugins

--- wp-content/plugins/ecwid-shopping-cart/js/landing.js
+++
@@ -1,5 +0,0 @@
-switch_to_connect = function() {
-	jQuery('.ecwid-landing').removeClass('register').addClass('connect');
-	jQuery('.ecwid-thank-step-one').addClass('active');
-	wpCookies.set('ecwid_create_store_clicked', 1);
-}
--- wp-content/plugins/ecwid-shopping-cart/js/themes/2012.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function() {
-	Ecwid.OnPageLoaded.add(function() {
-		jQuery('.ecwid a').first().focus();
-	});
-});
